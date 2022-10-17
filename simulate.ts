5b7ad9dae8b2da1f4303fe55d30bac9a2752763a
10K <bretthoganpr@gmail.com>
2022-10-16 20:04:33 -0400

    Added the z array as current agent

--- simulate.ts
+++ simulate.ts
@@ -4,20 +4,13 @@ import { NormalizeArray, ArrayAvg } from "./misc";
 
 //Simulator
 export function GetAgentScores(session: SessionClass) {
-    const scores = session.Agents.map(Agent => {
-        return { score: GetAgentScore(session, Agent), agent: Agent }
-    })
 
-    return scores
-}
+    // Agent, node data group, node 
+    let nodes: number[][][] = session.Agents.map(e => session.Data.map(e => e.input))
 
-export function GetAgentScore(session: SessionClass, Agent: AgentClass): number {
-    //function (inputCount: number, nodes: number[][], Weights: number[][][]) {
-    //y = node group; x = outnode;
-
-    let nodes: number[][] = session.Data.map(e => e.input)
-
-    for (let layer = 0; layer < Agent.WeightTable.length; layer++)
+    for (let layer = 0; layer < session.Layers.length - 1; layer++) {
+        // Agent, input node, output node
+        const Weights: number[][][] = session.Agents.map(e => e.WeightTable[layer]);
         for (let section = 0; section < session.Data.length; section += session.DataBatch) {
             const remainer = session.Data.length - section;
             const sectionLength = remainer < session.DataBatch ? remainer : session.DataBatch;
@@ -25,31 +18,37 @@ export function GetAgentScore(session: SessionClass, Agent: AgentClass): number 
             (simThread
                 .setOutput([
                     session.Layers[layer + 1],
-                    sectionLength
+                    sectionLength,
+                    session.Agents.length
                 ])(
                     session.Layers[layer],
-                    nodes.slice(section, sectionLength + section),
-                    Agent.WeightTable[layer]
-                ) as number[][]).forEach((e, i) => { nodes[i + section] = e })
+                    nodes.map(e => e.slice(section, sectionLength + section)),
+                    Weights
+                ) as number[][][]).forEach((e, agenti) => e.forEach((e, i) => { nodes[agenti][i + section] = e }))
         }
+    }
 
-    const scores = nodes.map((output, i) => {
-        //TODO: changeable score system
-        const result = NormalizeArray(output)
+    const scores = nodes.map((e, agentI) => {
+        const scores = e.map((output, i) => {
+            //TODO: changeable score system
+            const result = NormalizeArray(output)
 
-        return result[session.Data[i].output]
-    });
-    return ArrayAvg(scores)
+            return result[session.Data[i].output]
+        });
+        return { score: ArrayAvg(scores), agent: session.Agents[agentI] }
+    })
+
+    return scores
 }
 
 const gpu = new GPU({ mode: "gpu" })
 
-const simThread = gpu.createKernel(function (inputCount: number, nodes: number[][], Weights: number[][]) {
-    //y = node group; x = outnode;
+const simThread = gpu.createKernel(function (inputCount: number, nodes: number[][][], Weights: number[][][]) {
+    //z = agent; y = node group; x = outnode;
 
     let sum = 0
     for (let i = 0; i < inputCount; i++)
-        sum += nodes[this.thread.y][i] * Weights[i][this.thread.x]
+        sum += nodes[this.thread.z][this.thread.y][i] * Weights[this.thread.z][i][this.thread.x]
 
     return sum
 }).setDynamicOutput(true).setDynamicArguments(true)
